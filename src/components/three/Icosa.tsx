96dc489884563ba36091b5cab8ce0e822e1e78f6
nemutas <inohiro.focru@gmail.com>
2022-01-05 23:21:23 +0900

    first commit

--- src/components/three/Icosa.tsx
+++ src/components/three/Icosa.tsx
@@ -64,11 +64,18 @@ export const Icosa: VFC = () => {
 }
 
 const vertexShader = `
+varying vec3 v_normal;
 varying vec3 v_eye;
+varying vec3 v_mvPos;
 
 void main() {
-  vec4 mPos = modelMatrix * vec4( position, 1.0);
+  v_normal = normalize(normalMatrix * normal);
+
+  vec4 mPos = modelMatrix * vec4(position, 1.0);
 	v_eye = normalize(mPos.xyz - cameraPosition);
+
+  vec4 mvPos = modelViewMatrix * vec4(position, 1.0);
+	v_mvPos = normalize(mvPos.xyz);
 
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
@@ -77,26 +84,28 @@ void main() {
 const fragmentShader = `
 uniform sampler2D u_texture;
 uniform float u_time;
+varying vec3 v_normal;
 varying vec3 v_eye;
+varying vec3 v_mvPos;
 
-// https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83#voronoise
 vec2 hash22( vec2 p ){
 	p = vec2( dot(p,vec2(127.1,311.7)),dot(p,vec2(269.5,183.3)));
 	return fract(sin(p)*43758.5453);
 }
 
 float Fresnel(vec3 eyeVector, vec3 worldNormal) {
-	return pow(1.0 + dot(eyeVector, worldNormal), 0.4);
+	return pow(1.0 + dot(eyeVector, worldNormal), 3.0);
 }
 
 void main() {
-  // https://wgld.org/d/webgl/w087.html
-  vec3 x = dFdx(v_eye);
-  vec3 y = dFdy(v_eye);
+  // frat shading
+  vec3 x = dFdx(v_normal);
+  vec3 y = dFdy(v_normal);
   vec3 normal = normalize(cross(x, y));
 
-  vec2 uv = gl_FragCoord.xy / vec2(1024.0);
+  vec2 uv = gl_FragCoord.xy / vec2(1000.0);
 
+  // uv switcher
   float diffuse = dot(normal, vec3(1.0));
   float periodicity = (sin(u_time) + 1.0) / 2.0;
   vec2 seed = vec2(floor(diffuse * 5.0 + periodicity * 1.5));
@@ -105,14 +114,15 @@ void main() {
   vec2 switcher = sign(rand) * 1.0 + rand * 0.6;
   uv *= switcher;
   
-  // https://tympanus.net/codrops/2019/10/29/real-time-multiside-refraction-in-three-steps/
+  // refract
   float ior = 1.45;
   vec3 refracted = refract(v_eye, normal, 1.0 / ior);
   uv += refracted.xy * 0.5;
 
   vec4 t = texture2D(u_texture, uv);
 
-  float fresnel = Fresnel(v_eye, normal);
+  // fresnel reflect
+  float fresnel = Fresnel(v_mvPos, normal);
   vec4 color = mix(t, vec4(1.0), fresnel);
 
   gl_FragColor = color;
